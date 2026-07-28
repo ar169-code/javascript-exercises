3856bcc534977092344653d8051768c35f94fe3f
AN <arngu01@outlook.com>
2026-07-28 22:46:40 +0100

    Complete task 09
    Completed task 09 which summed all numbers in a range.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if ((typeof(first) !== "number" || typeof(second) !== "number")
+    || (first < 0 || second < 0)
+    || (Math.floor(first) !== first || Math.floor(second) !== second)) {
+        return "ERROR"
+    }
 
+    let sum = 0;
+    let initial;
+    let final;
+
+    if (first < second) {
+        initial = first;
+        final = second;
+    } else {
+        initial = second;
+        final = first;
+    }
+
+    for (let i = initial; i <= final; i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
