a5caf53600b0820e60ce7afdca5f1058e3be7094
AN <arngu01@outlook.com>
2026-07-28 22:34:44 +0100

    Complete task 08
    Completed task 08 to remove items from an array.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    copiedArr = arr.slice()
+
+    for (item of items){
+        while (copiedArr.includes(item)) {
+            copiedArr.splice(copiedArr.indexOf(item),1)}}
+    
+    return copiedArr 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
