ab2bc5cdd0b1effcb0227f1305c1d4cb9d75d0d6
AN <arngu01@outlook.com>
2026-08-03 23:55:40 +0100

    Complete task 16
    Create a function to find the oldest person in an array dead or alive.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldest = array.reduce((oldest, item) => {
+        if (!oldest) {
+        return oldest = item;
+        }
+        
+        const oldestEndYear = oldest.yearOfDeath ? oldest.yearOfDeath : new Date().getFullYear();
+        const itemEndYear = item.yearOfDeath ? item.yearOfDeath : new Date().getFullYear();
 
+        if (oldestEndYear - oldest.yearOfBirth > itemEndYear - item.yearOfBirth) {
+            return oldest}
+
+    return oldest = item;
+    }, {})
+
+    return oldest
 };
 
 // Do not edit below this line
