5a120f400a661ef61f21cda4b39f26d9f22160bc
AN <arngu01@outlook.com>
2026-08-03 23:23:48 +0100

    Complete task 14
    Create a function to find a certain term in the fibonacci sequence.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(term) {
+    if (term < 0) {
+        return ("OOPS")
+    }
+    
+    // create array with initial values 
+    const fibArray = [0, 1, 1]
 
+    // create for loop to push preceding values up to the term.
+    for (let i = 3; i <= term; i++) {
+        fibArray.push(fibArray[i-1] + fibArray[i-2])
+    }
+
+    console.log(fibArray)
+
+    return fibArray[term]
 };
 
 // Do not edit below this line
