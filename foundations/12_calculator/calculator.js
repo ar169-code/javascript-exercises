adfdf49ebd1794671b0b77ad13e377ecf7288357
AN <arngu01@outlook.com>
2026-08-03 22:51:16 +0100

    Complete task 12
    Create functions for a calculator.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, item) => sum += item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, item) => sum *= item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let currentTotal = 1;
+	for (let i = num; i > 1; i--) {
+    currentTotal *= i;
+  };
+
+  return currentTotal;
 };
 
 // Do not edit below this line
