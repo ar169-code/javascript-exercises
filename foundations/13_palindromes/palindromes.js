706692814492fd1f4bf3dd2dc656ed53bb08f96f
AN <arngu01@outlook.com>
2026-08-03 23:11:15 +0100

    Complete task 13
    Check if an input was a palindrome.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"
 
+    const cleanedString = string.split("").filter((item) => alphabet.includes(item.toLowerCase())).join("")
+
+    const reversedString = cleanedString.split("").reverse().join("")
+
+    return reversedString.toLowerCase() === cleanedString.toLowerCase()
 };
 
 // Do not edit below this line
